20fd86bff9308d9c2f222bfab4a4b704f07e4185
شمس الدين سليمان خولاني <84361889+shamskholani2020@users.noreply.github.com>
2026-02-13 23:43:40 +0200

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,3 +7,6 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+// Export all your model types for easy import
+export type { Doctor, Availability, Appointment, AppointmentStatus, MessageStatus } from '@prisma/client'
